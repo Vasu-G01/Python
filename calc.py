c73ffdc126020d273f81ae1ec5b52dfd7d19d13a
Vasu Gumber <vasugumber463@gmail.com>
2025-05-18 15:35:00 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,14 @@
+def sum(a,b):
+	add=a+b
+	print("Addition : ",add)
+def sub(a,b):
+	sub=a-b
+	print("Subraction : ",sub)
+def mult(a,b):
+	mult=a*b
+	print("Multiplication : ",mult)
+a=int(input("Enter Number : "))
+b=int(input("Enter Number : "))
+sum(a,b)
+sub(a,b)
+mult(a,b)
